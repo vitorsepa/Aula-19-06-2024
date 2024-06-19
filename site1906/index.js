c89f5e3ff28d04a85cab95710976e731f86958b8
vitorsepa <162443534+vitorsepa@users.noreply.github.com>
2024-06-19 09:31:03 -0300

    Add files via upload

---
+++ site1906/index.js
@@ -0,0 +1,14 @@
+let topMod = 0;
+let leftMod = 0;
+
+$("body").on("mousemove", webHandler);
+
+function webHandler(event){
+    let button = $('#no').position();
+    let buttonCenter = {
+        x: button.left + 50,
+        y: button.top + 20
+    }
+
+    let distance = Math.sqrt(Math.pow())
+}
